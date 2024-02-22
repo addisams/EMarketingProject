67ffef1d7bd2c82f408f6fa30e080ed523f1b9cc
addisams <adsrivastav307@gmail.com>
2024-02-22 11:25:06 +0530

    Admin login created

---
+++ EMarketingProject/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EMarketingProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
